f6620c52e12ad0d302e3715ea786a11dda283e57
ThisIsSebi <shoff439@gmail.com>
2025-04-01 19:32:09 +0200

    Tried to make each bubble a certain colour

--- sketch.js
+++ sketch.js
@@ -2,14 +2,29 @@
 
 var nums = [100, 25, 46, 72];
 
+var col = {
+  r: 0,
+  g: 0,
+  b: 0
+}
+
+
 function setup() {
-  createCanvas(500, 400);
+ 
 }
 
 function draw() {
   background(0);
+  createBubbles()
+}
+
+function createBubbles(){
   var x = 100;
   for (var i = 0; i < 4; i++) {
+    col.r = random(0,255);
+    col.g = random(0,255);
+    col.b = random(0,255);
+    fill(col.r, col.g, col.b)
     ellipse(x, 200, nums[i], nums[i]);
     x += 100;
   }
