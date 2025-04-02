5118b6832f1781d55cea3d4c43939b7b54d656ce
ThisIsSebi <shoff439@gmail.com>
2025-04-02 21:14:54 +0200

    Bubbles is shown in different sizes randomly

--- sketch.js
+++ sketch.js
@@ -1,20 +1,22 @@
 // make each bubble a random colour
 
-var nums = [100, 25, 46, 72];
-
-var randomColor;
-
 function setup() {
+  var num1 = random(200);
+  var num2 = random(200);
+  var num3 = random(200);
+  var num4 = random(200);
+  var nums = [num1, num2, num3, num4];
 
-   //tiny canvas
-   createCanvas(500, 500);
-  
-   //white background
-   background(0);
+  var randomColor;
+  //tiny canvas
+  createCanvas(500, 500);
+
+  //white background
+  background(0);
   var x = 100;
-  for (var i = 0; i < 4; i++) {
-    randomColor = color(random(255), random(255), random(255))
-    fill(randomColor)
+  for (var i = 0; i < nums.length; i++) {
+    randomColor = color(random(255), random(255), random(255));
+    fill(randomColor);
 
     ellipse(x, 200, nums[i], nums[i]);
     x += 100;
