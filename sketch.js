4522bfdb61ec0ca6b3d12a8d17866925e510e0b4
ThisIsSebi <shoff439@gmail.com>
2025-04-03 18:58:13 +0200

    tried to change colour when clicked on button

--- sketch.js
+++ sketch.js
@@ -6,19 +6,32 @@ function setup() {
   var num3 = random(200);
   var num4 = random(200);
   var nums = [num1, num2, num3, num4];
+  let button;
 
   var randomColor;
   //tiny canvas
   createCanvas(500, 500);
 
+  button = createButton('Click');
+
   //white background
   background(0);
   var x = 100;
   for (var i = 0; i < nums.length; i++) {
-    randomColor = color(random(255), random(255), random(255));
+
+    randomColor = button.mousePressed(changeColour)
     fill(randomColor);
 
     ellipse(x, 200, nums[i], nums[i]);
     x += 100;
   }
+
+  //button.position((windowWidth - button.width) / 8);
+  button.position(210,520)
+
+  button.size(50,30)
+}
+
+function changeColour(){
+  color(random(255), random(255), random(255));
 }
