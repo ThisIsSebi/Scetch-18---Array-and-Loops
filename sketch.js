7eef61c56556985d2e9dff34ec8bebddc10ce716
ThisIsSebi <shoff439@gmail.com>
2025-04-04 22:18:00 +0200

    Colour is changed when reloading page

--- sketch.js
+++ sketch.js
@@ -6,32 +6,37 @@ function setup() {
   var num3 = random(200);
   var num4 = random(200);
   var nums = [num1, num2, num3, num4];
+
   let button;
 
-  var randomColor;
   //tiny canvas
   createCanvas(500, 500);
 
-  button = createButton('Click');
+  button = createButton("Click");
 
   //white background
   background(0);
+
+  r = random(255);
+  g = random(255);
+  b = random(255);
   var x = 100;
+
   for (var i = 0; i < nums.length; i++) {
 
-    randomColor = button.mousePressed(changeColour)
-    fill(randomColor);
-
+    fill(r, g, b);
     ellipse(x, 200, nums[i], nums[i]);
     x += 100;
   }
 
+  button.mousePressed(changeColour);
   //button.position((windowWidth - button.width) / 8);
-  button.position(210,520)
+  button.position(210, 520);
 
-  button.size(50,30)
+  button.size(50, 30);
 }
 
-function changeColour(){
-  color(random(255), random(255), random(255));
+function changeColour() {
+  console.log("pressed");
+  return r = random(255), g = random(255), b = random(255);
 }
